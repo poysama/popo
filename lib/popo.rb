e750bb1c40473b537e0e1f0a338a8bb3ca2c4fe3
Mark <mjwork@simpleteq.com>
2010-07-13 18:12:46 +0800

    additional changes

--- lib/popo.rb
+++ lib/popo.rb
@@ -1,14 +1,34 @@
 # add additional commands for the originally loaded popo
-COMMANDS.concat %w{ sync }
+COMMANDS.concat %w{ info status sync }
 
 module Popo
-  def self.proxy_commands(root_path, opts, opts_parse, argv = [ ])
+  def self.commands(root_path, opts, opts_parse, argv = [ ])
     case argv[0]
     when 'sync'
       Popo.sync(root_path)
+    when 'info'
+      Popo.info(root_path)
+    when 'status'
+      Popo.status(root_path)
     else
-      opts_parse.help
+      puts opts_parse.help
     end
   end
-end
 
+  def self.check_extended_requirements!
+  end
+
+  def self.info(root_path)
+    info = {
+      "Work Path" => root_path,
+      "Target" => POPO_CONFIG["target"]
+    }
+
+    info.each do |k,v|
+      puts "#{k}: #{v}"
+    end
+  end
+
+  def self.status(root_path)
+  end
+end
