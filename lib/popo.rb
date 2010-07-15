5531276f30bc567aa1525a9281aee78372483ce9
Mark <mjwork@simpleteq.com>
2010-07-15 16:18:38 +0800

    added popo bash

--- lib/popo.rb
+++ lib/popo.rb
@@ -1,5 +1,9 @@
 # add additional commands for the originally loaded popo
-COMMANDS.concat %w{ info status sync }
+COMMANDS.concat %w{ bash info status sync }
+
+BASH_BIN = `which bash`.strip
+ENV_BIN = `which env`.strip
+POPORC = 'scripts/poporc'
 
 module Popo
   def self.commands(root_path, opts, opts_parse, argv = [ ])
@@ -10,6 +14,8 @@ module Popo
       Popo.info(root_path)
     when 'status'
       Popo.status(root_path)
+    when 'bash'
+      Popo.bash(root_path)
     else
       puts opts_parse.help
     end
@@ -32,5 +38,19 @@ module Popo
   def self.status(root_path)
     puts "Gathering status info, please be patient, this can take a while on a pathetic machine..."
     $stdout.flush
+  end
+
+  def self.bash(root_path)
+    if BASH_BIN.nil? || BASH_BIN.empty?
+      fail_exit! "FAIL bash is nowhere to be found"
+    end
+
+    if ENV_BIN.nil? || ENV_BIN.empty?
+      fail_exit! "FAIL env is nowhere to be found"
+    end
+
+    target = POPO_CONFIG['target']
+    bashcmd = "#{ENV_BIN} popo_target=#{target} popo_path=#{root_path} #{BASH_BIN} --rcfile #{File.join(root_path, POPO_WORK_PATH, POPORC)}"
+    exec(bashcmd)
   end
 end
